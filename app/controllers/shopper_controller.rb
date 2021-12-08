db91b6e36f0873a86d8a1c49dc33d30a718be287
harshitharbhat <harshitharbhat@gmail.com>
2021-12-08 02:27:50 -0800

    Adding css

--- app/controllers/shopper_controller.rb
+++ app/controllers/shopper_controller.rb
@@ -5,4 +5,13 @@ class ShopperController < ApplicationController
   def index
     @products = Product.order(:name)
   end
+
+  def search
+    if params[:search].blank?
+      redirect_to shopper_url and return
+    else
+      @parameter = params[:search].downcase
+      @results = Product.all.where("lower(name) LIKE :search", search: "%#{@parameter}")
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
   get 'shopper/index'
   root "shopper#index", as:'shopper' # root = get '/'
   resources :products
+  get '/search', to: "shopper#search"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
